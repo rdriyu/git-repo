7ce49ae44f7c55fb690d660d103967dbed87e417
Riya D <Riya D@DESKTOP-M1DKFQU>
2020-10-16 09:35:29 +0530

    Added Transaction Through Master

---
+++ OnlineShopping/src/com/lti/model/Transaction.java
@@ -0,0 +1,21 @@
+package com.lti.model;
+
+import java.time.LocalDate;
+
+public class Transaction {
+	int transactionid;
+	LocalDate transactiondate;
+	public int getTransactionid() {
+		return transactionid;
+	}
+	public void setTransactionid(int transactionid) {
+		this.transactionid = transactionid;
+	}
+	public LocalDate getTransactiondate() {
+		return transactiondate;
+	}
+	public void setTransactiondate(LocalDate transactiondate) {
+		this.transactiondate = transactiondate;
+	}
+
+}
